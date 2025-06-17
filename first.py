1c8f7562152e6b2941f182354c3532d6a4a846f4
richa-singh22 <swatipriya9060@gmail.com>
2025-06-17 13:50:39 +0530

    First commit

---
+++ first.py
@@ -0,0 +1,28 @@
+# Store the name of 5 peoples
+# generate some message with their name. 
+
+names = []
+# names = list()
+
+
+for i in range(5): # range(0,5)
+    # ... ellipses
+    names.append(input(f"Enter {i+1}st name: ").title())
+
+for name in names:
+    print(f"Welcome,{name}!")
+
+# print(names[0])
+
+# for(int i = 0; i < 5; i++)
+# range(n) -> 0 till n - 1
+# range(m, n) -> m till n - 1 m->start n-end
+# for(int i = 1; i <= 5; i++)
+# range(start,end,step) -> 1 to 6 -> s = 2 -> 1, 3, 5
+# for(int i = 1; i <= 5; i += 2)
+# for int number : nums{sop(number)}
+# for fruit in fruits:
+#    print(fruit)
+
+# print(type(...))
+
